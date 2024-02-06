dc0ad67b1a2b6168a8ae991c078b91c8bde96456
Justfemi <ajibade.olufemi@yahoo.com>
2024-02-06 05:30:53 +0100

    feat:project setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <a href="/">
+        <h2>DEVLOOP <br /> BLOG</h2>
+      </a>
+
+      <div>
+        <a>HOME</a>
+        <a>ABOUT ME</a>
+        <a>DROPDOWN</a>
+        <a>BLOG</a>
+        <a>CONTACT ME</a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
